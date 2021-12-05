f12c546992a0173d903e383f61056e2be982b6de
David Hu <b01901133@gmail.com>
2021-12-04 20:16:43 -0500

    restructure project folders

--- client/src/components/Root/Root.js
+++ client/src/components/Root/Root.js
@@ -1,10 +1,10 @@
-import React from "react";
-import { Provider as ReduxProvider } from "react-redux";
-import { BrowserRouter as Router } from "react-router-dom";
+import React from 'react';
+import { Provider as ReduxProvider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-import store from "../../application/redux/store";
-import { AuthProvider } from "../../firebase/Auth";
-import App from "../App";
+import store from '../../application/redux/store';
+import { AuthProvider } from '../../firebase/Auth';
+import App from '../App';
 
 export default function Root() {
   return (
